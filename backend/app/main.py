ade8ef1237bd94aa095dfab09ba88180dcc8d988
shubhamkgupta14-test <shubham.kumarcs15@gmail.com>
2026-06-27 12:02:24 +0530

    Add frontend CORS URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,7 +55,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://your-frontend.vercel.app"
+        "https://happihome-inventory.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
